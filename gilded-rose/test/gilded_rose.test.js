c22aec131448f10be4ed71095bf37c5809f9492d
Adam Comella <rigdern@gmail.com>
2022-01-27 09:27:56 -0800

    Add a starting point for tests

--- gilded-rose/test/gilded_rose.test.js
+++ gilded-rose/test/gilded_rose.test.js
@@ -1,9 +1,43 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
+const ItemType = {
+  Sulfuras: 'Sulfuras, Hand of Ragnaros',
+};
+
+const sulfurasQuality = 80;
+
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  // Sulfuras item tests
+  // 
+
+  it("should not modify the sell in of a Sulfuras item", function() {
+    const quality = sulfurasQuality;
+    const sellIn = 5;
+    let items = [new Item(ItemType.Sulfuras, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = 0; i < 10; ++i) {
+      items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(sellIn);
+    }
   });
+
+  // TODO: Add more tests
+  //
+
 });
+
+// Example items
+//
+// const items = [
+//   new Item("+5 Dexterity Vest", 10, 20),
+//   new Item("Aged Brie", 2, 0),
+//   new Item("Elixir of the Mongoose", 5, 7),
+//   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+//   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+//   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+//   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+//   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+
+//   // This Conjured item does not work properly yet
+//   new Item("Conjured Mana Cake", 3, 6),
+// ];
